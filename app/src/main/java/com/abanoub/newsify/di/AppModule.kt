3b5679fed1340365930de6e60c0da4feadfcfad9
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-30 00:48:07 +0300

    Setup Hilt AppModule

---
+++ app/src/main/java/com/abanoub/newsify/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.abanoub.newsify.di
+
+import android.content.Context
+import androidx.room.Room
+import com.abanoub.newsify.data.local.ArticleDao
+import com.abanoub.newsify.data.local.NewsDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(@ApplicationContext context: Context): NewsDatabase {
+        return Room.databaseBuilder(
+            context,
+            NewsDatabase::class.java,
+            "news_db.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideArticleDao(database: NewsDatabase): ArticleDao {
+        return database.articleDao()
+    }
+}
