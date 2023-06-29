57d94bb413c4abf54357948185dad84b80099499
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-29 22:43:54 +0300

    Setup API Response

---
+++ app/src/main/java/com/abanoub/newsify/domain/model/Source.kt
@@ -0,0 +1,8 @@
+package com.abanoub.newsify.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id") var id: String? = null,
+    @SerializedName("name") var name: String? = null
+)
