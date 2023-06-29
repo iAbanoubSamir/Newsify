0b21139fa25b43afa99ae7b8b2e7e98e2314e123
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-30 01:05:21 +0300

    Setup FavouriteRepository with Hilt

--- app/src/main/java/com/abanoub/newsify/di/RepositoryModule.kt
+++ app/src/main/java/com/abanoub/newsify/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package com.abanoub.newsify.di
 
+import com.abanoub.newsify.data.local.ArticleDao
+import com.abanoub.newsify.data.mapper.ArticleMapper
 import com.abanoub.newsify.data.remote.NewsApiService
+import com.abanoub.newsify.data.repository.FavouriteRepositoryImpl
 import com.abanoub.newsify.data.repository.NewsRepositoryImpl
+import com.abanoub.newsify.domain.repository.FavouriteRepository
 import com.abanoub.newsify.domain.repository.NewsRepository
 import dagger.Module
 import dagger.Provides
@@ -17,5 +21,14 @@ object RepositoryModule {
     @Singleton
     fun provideNewsRepository(newsApiService: NewsApiService): NewsRepository {
         return NewsRepositoryImpl(newsApiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavouriteRepository(
+        articleDao: ArticleDao,
+        articleMapper: ArticleMapper
+    ): FavouriteRepository {
+        return FavouriteRepositoryImpl(articleDao, articleMapper)
     }
 }
