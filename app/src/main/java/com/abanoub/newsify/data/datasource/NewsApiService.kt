c24004cd311393a9c1b81bda96f7fb6fdda5348a
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-29 22:44:11 +0300

    Create API Service

---
+++ app/src/main/java/com/abanoub/newsify/data/datasource/NewsApiService.kt
@@ -0,0 +1,30 @@
+package com.abanoub.newsify.data.datasource
+
+import com.abanoub.newsify.domain.model.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApiService {
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "06010402445546f1ba2dbeae09d44f38"
+    }
+
+    @GET("v2/top-headlines")
+    suspend fun getNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        page: Int
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchNews(
+        @Query("q")
+        query: String,
+        @Query("page")
+        page: Int
+    ): Response<NewsResponse>
+}
