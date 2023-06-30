4646142b5e206efbf0bff9f550247081355644e6
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-30 14:15:05 +0300

    Implement Navigation

--- app/src/main/java/com/abanoub/newsify/presentation/MainActivity.kt
+++ app/src/main/java/com/abanoub/newsify/presentation/MainActivity.kt
@@ -2,13 +2,45 @@ package com.abanoub.newsify.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.abanoub.newsify.R
 import com.abanoub.newsify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfiguration = AppBarConfiguration.Builder(
+            setOf(
+                R.id.homeFragment
+            )
+        ).build()
+
+        setSupportActionBar(binding.mainToolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
